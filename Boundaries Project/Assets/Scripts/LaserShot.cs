1373ec9aeb7020c6089b35b5d226b09a5ea16e81
ChurrosBlack <andrebarbosa664@gmail.com>
2016-05-18 03:07:59 -0300

    All lasers set

--- Boundaries Project/Assets/Scripts/LaserShot.cs
+++ Boundaries Project/Assets/Scripts/LaserShot.cs
@@ -7,33 +7,43 @@ public class LaserShot : MonoBehaviour
     /// Atira laser via Raycast para cima, de timeToshoot em timeToShoot
     /// Quando atira o laser permanece visível por timeShooting segundos
     /// </summary>
-   public float timeToShoot;
-   public float timeShooting = 3f;
+   public float timeShoting = 3f;
+   public float timeToShot = 3f;
    public float auxTime;
    public float shotDistance = 10f;
    public LayerMask playerLayer;
    GameOverManager gameOverManager;
+   bool shooting = false;
     void Start()
     {
-        auxTime = timeToShoot;
+        auxTime = timeShoting;
         gameOverManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameOverManager>();
     }
 
     void Update()
     {
-        timeToShoot -= Time.deltaTime;
-        if (timeToShoot<=0)
+        if (!shooting)
         {
-            Shoot();
-            StartCoroutine(Wait(timeShooting));
-            timeToShoot = auxTime;
+            timeShoting -= Time.deltaTime;
+            if (timeShoting <= 0)
+            {
+                shooting = true;
+                StartCoroutine(Wait());
 
+            }
+            else
+            {
+                Shoot();
+            }
         }
+        
     }
 
     void Shoot()
     {
-        if (Physics2D.Raycast(transform.position, Vector2.up, shotDistance, playerLayer))
+        print("shot");
+        Debug.DrawRay(transform.position, transform.up * shotDistance, Color.yellow);
+        if (Physics2D.Raycast(transform.position, transform.up, shotDistance, playerLayer))
         {
             print("Cant go bro, look at that drop right there nigguh");
             gameOverManager.boyDead = true;
@@ -41,8 +51,11 @@ public class LaserShot : MonoBehaviour
         }
     }
 
-   public IEnumerator Wait(float time)
+   public IEnumerator Wait()
     {
-        yield return new WaitForSeconds(time);
-    }
+
+        yield return new WaitForSeconds(timeToShot);
+        timeShoting = auxTime;
+        shooting = false;
+   }
 }
